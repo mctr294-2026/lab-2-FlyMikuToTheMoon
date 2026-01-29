9c0f068b46c1c57f5ea6f8a0bdea5e68da8f78da
Jinton Chang <jinton@ualberta.ca>
2026-01-28 18:29:26 -0700

    Added the other 3 root methods

--- src/roots/roots.cpp
+++ src/roots/roots.cpp
@@ -1,27 +1,27 @@
 #include "roots.hpp"
 #include <cmath>
 
-double precision = 1e-6;
+double precision = 1e-6; // specified precision required for the root to satisfy
 
 bool bisection(std::function<double(double)> f, double a, double b, double *root) {
    
     double c = (a+b)/2;
     
-    if (f(a)*f(b) > 0) {
+    if (f(a) * f(b) > 0) { // checking if f(a) and f(b) have opposite signs
 
-        return false;
+        return false; // no root exists if they don't have opposite signs
 
     } else {
 
-        while (std::abs(f(c)) > precision) {
+        while (std::abs(f(c)) > precision) { // checking if the root has a high enough precision
             
-            c = (a+b)/2;
+            c = (a + b)/2;
 
-            if (f(a)*f(c) < 0) {
+            if (f(a) * f(c) < 0) {
 
                 b = c;
 
-            } else if (f(b)*f(c) < 0) {
+            } else if (f(b) * f(c) < 0) {
 
                 a = c;
 
@@ -32,57 +32,90 @@ bool bisection(std::function<double(double)> f, double a, double b, double *root
         }
 
         *root = c;
-        
         return true;
     } 
     
 }
 
-bool regula_falsi(std::function<double(double)> f,
-                  double a, double b,
-                  double *root) {
+bool regula_falsi(std::function<double(double)> f, double a, double b, double *root) {
 
-    double c = a - (f(a)*(b-a))/(f(b)-f(a));
-  
-    if (f(c) == 0) {
+    double c = a - (f(a) * (b - a)) / (f(b) - f(a));
+    
+    if (f(a) * f(b) > 0) {
 
-    return true;
+        return false;
 
-   } else {
+    } else {
 
-    return false;
-   }    
+        while (std::abs(f(c)) > precision) {
+            
+            c = a - (f(a) * (b-a)) / (f(b) - f(a));
+
+            if (f(a) * f(c) < 0) {
+
+                b = c;
+
+            } else if (f(b) * f(c) < 0) {
+
+                a = c;
+
+            }
+        }
+
+        *root = c;
+        return true;
+    }    
 }
 
 bool newton_raphson(std::function<double(double)> f,
                     std::function<double(double)> g,
                     double a, double b, double c,
                     double *root) {
-    if (g == 0) {
+    
+    if (f(a) * f(b) > 0) {
+
+        return false;
+
+    } else if (g == 0) {
 
         return false;
 
     } else {
 
-        c = b;
+        c = a;
 
-        while (f(c) != 0) {
-            c = c - f(c)/g(c);
+        while (std::abs(f(c)) > precision) {
+
+            c = c - f(c) / g(c);
+
         }
 
         return true;
     }
-        
 }
 
-bool secant(std::function<double(double)> f,
-            double a, double b, double c,
-            double *root) {
-    
-    if (f(a) != 0) {
+bool secant(std::function<double(double)> f, double a, double b, double c, double *root) {
+
+    if (f(a) * f(b) > 0) {
+
         return false;
+
     } else {
+
+        while (std::abs(f(c)) > precision) {
+
+            c = b - ((a - b) / (f(a) - f(b))) * f(b);
+            
+            a = b; // make sure to update a's value first before updating b, or you may end ip
+                   // dividing by zero
+            b = c;
+
+        }
+        
+        *root = c;
+
         return true;
+
     }
 }
 
